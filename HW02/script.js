8412725dff9a583d47a7d57f61888b4806f9d3d6
Taras Radevych <taras.radevych@gmail.com>
2023-03-05 18:44:45 +0200

    HW02_виправлення помилки

--- HW02/script.js
+++ HW02/script.js
@@ -1,16 +1,14 @@
 let N = +prompt('введіть N:')
-let M = +prompt('введіть N:')
+let M = +prompt('введіть M:')
 let isEven = confirm('Пропускати парні числа?')
 let sum = 0
 
-if (!(N % 1 == 0)) {
+if (!(N % 1 == 0) || N === ' ') {
     alert('Число N не підходить!')
 };
-if (!(M % 1 == 0)) {
+if (!(M % 1 == 0) || M === ' ') {
     alert('Число M не підходить!')
 };
-
-
 
 if (isEven == true) {
 for (let i = N; i <M; i++ && !(i % 2 == 0)){
@@ -26,6 +24,9 @@ for (let i = N; i <M; i++ && !(i % 2 == 0)){
         }
         
     }
+
+
+
 
  console.log(sum)
 document.writeln('Введене число N: ', N)
