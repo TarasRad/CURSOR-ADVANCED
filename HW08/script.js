d188df5548aa68f1dfbd66578e5500cb458abd32
Taras Radevych <taras.radevych@gmail.com>
2023-03-29 00:58:57 +0300

    HW08_w/o advanced

--- HW08/script.js
+++ HW08/script.js
@@ -5,39 +5,48 @@ class Student {
     this.course = course;
     this._marks = marks;
     this.dismissed = false;
+    // this._null = null;
+    // this.recovered = true;
   }
   getInfo() {
     console.log(`${this.course} ${this.university}, ${this.name}`);
   }
   getAverageMark() {
-    console.log("temp", this.marks);
+    //console.log("temp", this.marks);
+    if(this.marks.length > 0){
     let sum = 0;
     let average = 0;
     for (let i = 0; i < this.marks.length; i++) {
       sum += this.marks[i];
       average = sum / this.marks.length;
     }
-    console.log(average);
+    return average
+    //console.log(average);
   }
+}
 
   dismiss() {
     this.dismissed = true;
+    this.marks = null
+    console.log('dismissed',this.dismissed);
+  }
+  recover() {
+    this.dismissed = false;
   }
 
+  get marks() {
+    if (!this.dismissed) {
+      return this._marks;
+    } else {
+      return null;
+    }
+  }
   set marks(mark) {
     if (!this.dismissed) {
-      return null;
-    } else {
-      this._marks.push(mark);
+        this._marks.push(mark);
     }
   }
-  get marks() {
-    if (this.dismissed) {
-      return null;
-    } else {
-      return this._marks;
-    }
-  }
+  
 }
 let stud = new Student(
   "Остап Родоманський Бендер",
@@ -51,4 +60,26 @@ console.log(stud.marks);
 stud.marks = 5;
 console.log(stud.marks);
 stud.getAverageMark();
-//stud.dismiss();
+console.log(stud.getAverageMark());
+stud.dismiss();
+console.log(stud.marks); // null
+stud.recover();
+console.log(stud.marks); 
+
+
+// class BudgetStudent extends Student {
+//     constructor(scholarship){
+//         super(scholarship)
+//     }
+//     getScholarship(){
+//         console.log('Виотримали 1400 грн. стипендії');
+
+//     }
+    
+// }
+// let newStud = new BudgetStudent()
+
+// console.log(newStud);
+
+// newStud.getScholarship()
+//console.log(newStud.getScholarship());
