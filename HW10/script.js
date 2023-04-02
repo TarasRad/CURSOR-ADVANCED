b19298a3d3a01e02b44b4229dec7fe94f93eb118
Taras Radevych <taras.radevych@gmail.com>
2023-04-03 00:08:09 +0300

    HW10_start

---
+++ HW10/script.js
@@ -0,0 +1,18 @@
+// const key = document.getElementsById("key1");
+// key.addEventListener("click", myFunction);
+// function myFunction () {
+//   console.log("click");
+// };
+const element = document.getElementById("key1");
+element.addEventListener("click"||"keydown", function() {
+    
+    console.log('click');
+  //document.getElementById("demo").innerHTML = "Hello World";
+});
+// document.eventTarget.addEventListener("keydown", (event) => {
+//     if (event.isComposing || event.keyCode === 229) {
+//       return;
+//     }
+//     console.log('click');
+//     // do something
+//   });
