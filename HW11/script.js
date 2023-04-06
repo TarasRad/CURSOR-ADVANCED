ef47709ed865c56d00b90e090b32ca0060e57b56
Taras Radevych <taras.radevych@gmail.com>
2023-04-07 00:51:07 +0300

    HW11_error fixed

--- HW11/script.js
+++ HW11/script.js
@@ -1,5 +1,5 @@
 function getRandomChinese(length) {
-//new Promise((resolve, reject)=>{
+new Promise((resolve, reject)=>{
 let i=0;
 let result ='';
 while (i<length){
@@ -9,47 +9,40 @@ while (i<length){
     console.log(number);
     result+=number
     i++
-    // if (i===length){
-    //     resolve(result)
-    // } else {
-    //     //setTimeout(getRandomNumber, 50);
-    // }
+    if (i===length){
+  resolve(result)
+    } else {
+        setTimeout(getRandomNumber,50)
+    }
 
   }
 
 getRandomNumber(); 
-};
 }
-getRandomChinese(4)
-// .then(result => {
-//     console.log(result);
-//   }).catch(error => {
-//     console.error(error);
-//   });
+})
 
+}
 
-/*
 function getRandomChinese(length) {
-  let result = '';
-  let i = 0;
-
-  const generateCharacter = () => {
-    return new Promise((resolve) => {
-      const sign = String(Date.now()).slice(-5);
-      const character = String.fromCharCode(sign);
-      setTimeout(() => {
-        resolve(character);
-      }, 50);
-    });
-  };
-
-  while (i < length) {
-    result += generateCharacter();
-    i++;
-  }
-
-  return Promise.all(result).then((result) => result.join(''));
-}
-*/
-
-// const number = String.fromCharCode(Math.floor(Math.random() * (40959 - 19968 + 1) + 19968));
+    return new Promise((resolve, reject)=>{
+    let i=0;
+    let result ='';   
+      function getRandomNumber() {
+        const number = String.fromCharCode(Date.now()).slice(-5);
+        console.log(number);
+        result+=number
+        i++
+        if (i===length){
+      resolve(result)
+        } else {
+            setTimeout(getRandomNumber,50)
+        }
+    
+      }
+    
+    getRandomNumber(); 
+    
+    })
+    
+    }
+    getRandomChinese(4).then((result) => console.log(result));
