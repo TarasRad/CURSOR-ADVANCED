087acd77e185da25a58febc909fb23c5b0890d06
Taras Radevych <taras.radevych@gmail.com>
2023-04-11 00:24:55 +0300

    HW12_v1.1

--- HW12/script.js
+++ HW12/script.js
@@ -1,25 +1,67 @@
-function getPeople(name){
-console.log(name);
+function getPerson({ name }) {
+  console.log(name);
 }
 
-
-function getInfo(){
-fetch("https://swapi.dev/api/films/").then((res)=>{
-return res.json();
-}).then(data=> {
-    console.log({data});
-})
-}
-getInfo()
-
-
-function getChatacters(){
-    fetch("https://swapi.dev/api/people/").then((res)=>{
-    return res.json();
-    }).then(people=> {
-        console.log({people});
-        getPeople()
-
+function getFilms() {
+  fetch("https://swapi.dev/api/films/")
+    .then((res) => {
+      return res.json();
     })
-    }
-    getChatacters()
+    .then((data) => {
+      console.log({ data });
+    });
+}
+getFilms();
+
+function getAllChatacters() {
+  const peopleUrl = "https://swapi.dev/api/people/";
+  const allPleople = [];
+  function getCharacters(url) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        allPleople.push(...data.results);
+        if (data.next) {
+          getCharacters(data.next);
+        } else {
+          console.log(allPleople);
+        }
+      });
+  }
+  getCharacters(peopleUrl);
+}
+getAllChatacters();
+
+function getFilmsCharacters(film) {
+  const filmsApi = "https://swapi.dev/api/films/";
+  let newUrl = filmsApi + film;
+  console.log("NEW URL", newUrl);
+  fetch(newUrl)
+    .then((res) => {
+      return res.json();
+    })
+    .then((film) => {
+      console.log(film.title);
+      const characters = film.characters;
+      console.log(characters);
+      let charactersByName = [];
+
+      for (let i = 0; i < characters.length; i++) {
+        let characterURL = characters[i];
+        fetch(characterURL)
+          .then((res) => res.json())
+          .then((character) => {
+            charactersByName.push([character.name, character.birth_year, character.gender]);
+            // charactersByName={
+            //     name:character.name
+            // }
+          });
+      }
+      setTimeout(() => {
+        console.log(charactersByName);
+      }, 500);
+    });
+}
+getFilmsCharacters(2);
+
+
