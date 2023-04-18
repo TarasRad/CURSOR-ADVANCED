e0736e1d8615e734bdd3e3cf797fa857cf01f45b
Taras Radevych <taras.radevych@gmail.com>
2023-04-18 23:03:35 +0300

    HW13_v1.01

--- HW13/script.js
+++ HW13/script.js
@@ -1,12 +1,16 @@
  const btnInc = document.getElementById("btnInc");
  const btnDec = document.getElementById("btnDec");
 
+const element = document.querySelector("p");
+const styles = window.getComputedStyle(element);
+let fontSize = parseFloat(styles.getPropertyValue('font-size'));
+console.log(fontSize); 
 
 function* infitiniteGenerator1(){
-  let initialID = 14;
+  // let initialID = 14;
   for (let i = 0; i < Infinity; i++) {
-    initialID +=2;
-    yield initialID;
+    fontSize +=1;
+    yield fontSize;
   }
 }
 const idGenerator1 = infitiniteGenerator1();
@@ -22,23 +26,33 @@ btnInc.addEventListener("click", () => {
   console.log(idGenerator1.next().value);
 });
 
-// function* infitiniteGenerator2(){
-//   let initialID=14;
 
-//   for (let i = 0; i < 2; i++) {
-//     initialID -=2;
-//     yield initialID;
-//   }
-// }
-// const idGenerator2 = infitiniteGenerator2();
-
-// //const btnInc = document.querySelector("button");
 
 // btnDec.addEventListener("click", () => {
 //   //infitiniteGenerator.next();
-//   idGenerator2.next();
-//   let value = idGenerator2.next().value;
+//   idGenerator1;
+//   let value = idGenerator1.value;
 //   const text = document.querySelector("p")
-//   text.style.fontSize = value+"px";
-//   console.log(idGenerator2.next().value);
+//   text.style.fontSize = (value-2)+"px";
+//   //console.log(idGenerator1.next().value);
 // });
+function* infitiniteGenerator2(){
+
+  for (let i = fontSize; i >0; i++) {
+    fontSize -=1;
+    //console.log(fontSize);
+    yield fontSize;
+  }
+}
+const idGenerator2 = infitiniteGenerator2();
+
+//const btnInc = document.querySelector("button");
+
+btnDec.addEventListener("click", () => {
+  //infitiniteGenerator.next();
+  idGenerator2.next();
+  let value = idGenerator2.next().value;
+  const text = document.querySelector("p")
+  text.style.fontSize = value+"px";
+  console.log(idGenerator2.next().value);
+});
